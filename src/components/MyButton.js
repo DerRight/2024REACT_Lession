c4be99d4de8c7361dce755e76645da1c72ea17d1
DerRight <zxc123456789123456@gmail.com>
2024-06-12 17:28:11 +0800

    新增計算機面板

---
+++ src/components/MyButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './calculator.css';
+
+function MyButton() {
+    return(
+        <div className="buttons">
+            機算機按鈕面板
+        </div>
+    );
+}
+export default MyButton;
---
+++ src/components/MyCalculator.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MyDisplay from "./MyDisplay";
+import MyButton from "./MyButton";
+import './calculator.css';
+
+function MyCalculator() {
+    return(
+        <div className="calculator">
+            <MyDisplay />
+            <MyButton />
+        </div>
+    );
+}
+export default MyCalculator;
